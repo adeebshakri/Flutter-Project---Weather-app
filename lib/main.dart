ecaea2d1ce71d63654f47dfc534b94910ce6724f
adeebshakri_ <adeebshakri7@gmail.com>
2020-05-23 11:30:34 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'klimatic.dart';
+import 'utils.dart';
+void main(){
+  runApp(
+      new MaterialApp(
+        title: 'Klimatic 2.0',
+        home: new Klimatic(),
+      )
+  );
+}
---
+++ lib/utils.dart
@@ -0,0 +1,2 @@
+final appId = "c7ce31b0db3efb6fa77e87e33dd6b7b4";
+final defaultCity = "Moscow";
